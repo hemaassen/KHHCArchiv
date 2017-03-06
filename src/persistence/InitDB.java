1d217dab3c2fe1284fda8257652d2c487d4f2d1d
telenair <telenair@web.de>
2017-03-06 19:27:19 +0100

    InitDB: Vor dem Anlegen der Tabellen, diese löschen, wenn bereits vorhanden

--- src/persistence/InitDB.java
+++ src/persistence/InitDB.java
@@ -16,17 +16,26 @@ public class InitDB {
 			System.out.println("Opened database successfully");
 			String sql;
 			stmt = c.createStatement();
+			sql = "DROP Table If EXISTS config"; //holger
+			stmt.executeUpdate(sql);             //holger
+			
 			sql = "CREATE TABLE config " + "( '_id' INTEGER PRIMARY_KEY  AUTO_INCREMENT   NOT NULL,"
 					+ " sourceRoot           VARCHAR(255)    NOT NULL, " + " destinationRoot            VARCHAR(255)     NOT NULL, "
 					+ " manualStore        CHAR(5) DEFAULT true)";
 			stmt.executeUpdate(sql);
 			stmt.close();
 			
+			sql = "DROP Table If EXISTS keywords"; //holger
+			stmt.executeUpdate(sql);               //holger
+			
 			sql = "CREATE TABLE keywords " + "( 'id' INTEGER PRIMARY KEY  AUTOINCREMENT,"
 					+ " keyWord           VARCHAR(50)    NOT NULL, " + " pathName            VARCHAR(50)     NOT NULL, "
 					+ " level     INTEGER  NOT NULL)";
 			stmt.executeUpdate(sql);
 			stmt.close();
+			
+			sql = "DROP Table If EXISTS childParent";  //holger
+			stmt.executeUpdate(sql);                   //holger
 			
 			sql = "CREATE TABLE childParent " + "( 'id' INTEGER PRIMARY KEY AUTOINCREMENT,"
 					+ " child    INTEGER  NOT NULL, " + " parent     INTEGER  NOT NULL, " + " FOREIGN KEY(child) REFERENCES keywords(id),"
