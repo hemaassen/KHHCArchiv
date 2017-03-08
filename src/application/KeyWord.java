7180d8717139341e61dfbd2a55f80fede2b7504b
KKruse8265 <Kerstin.Kruse-Wustrow@web.de>
2017-03-08 09:43:02 +0100

    KeyWord.compareTo auf Sonderfall "Neuer Eintrag.." angepasst JavaDocs hinzugefügt

--- src/application/KeyWord.java
+++ src/application/KeyWord.java
@@ -1,15 +1,15 @@
 package application;
 
-public class KeyWord implements Comparable<KeyWord>{
+public class KeyWord implements Comparable<KeyWord> {
 	private Integer id;
-	private String keyword;  	//Schlüsselwort
-	private String path;		//ein eventuell abweichender Pfad vom Schlüsselwort
-								//in Version 1=keyword
-	private Integer parent;		//die Id des übergeordneten Schlüsselwortes
-	private int level;		//die dem Schlüsselwort zugeordnete Ebene
-	
-	//Getter und Setter
-	
+	private String keyword; // Schlüsselwort
+	private String path; // ein eventuell abweichender Pfad vom Schlüsselwort
+							// in Version 1=keyword
+	private Integer parent; // die Id des übergeordneten Schlüsselwortes
+	private int level; // die dem Schlüsselwort zugeordnete Ebene
+
+	// Getter und Setter
+
 	public Integer getId() {
 		return id;
 	}
@@ -49,12 +49,11 @@ public class KeyWord implements Comparable<KeyWord>{
 	public void setLevel(Integer level) {
 		this.level = level;
 	}
-	
-	//Konstruktoren
+
+	// Konstruktoren
 	public KeyWord() {
 		// TODO Auto-generated constructor stub
 	}
-	
 
 	public KeyWord(Integer id, String keyword, String path, Integer parent, int level) {
 		super();
@@ -64,20 +63,33 @@ public class KeyWord implements Comparable<KeyWord>{
 		this.parent = parent;
 		this.level = level;
 	}
-
+	/**
+	 * in der ObserverableList wird bei der Anzeige in der Combobox auf die String-repräsentation
+	 * der List-Elemente zugegriffen. Darum hier eine absolute Kurzvariante. 
+	 * @author Kerstin
+	 */
 	@Override
 	public String toString() {
 		return keyword;
 	}
+	/**
+	 * die "natürlich Ordnung" der Klasse KeyWord wird über das
+	 * Attribut keywort geregelt. Hier wird dann die alphabetische 
+	 * Reihenfolge genutzt. Ausnahme ist "Neuer Eintrag.." der immer am Ende 
+	 * der Liste steht 
+	 * @author Kerstin
+	 */
 	@Override
 	public int compareTo(KeyWord k) {
-		int result=0;
-		result=keyword.compareTo(k.getKeyword());
-		
+		int result = 0;
+		if (keyword.equals("Neuer Eintrag..")) {
+			result = 1;
+		} else if (k.getKeyword().equals("Neuer Eintrag..")) {
+			result = -1;
+		} else {
+			result = keyword.compareTo(k.getKeyword());
+		}
 		return result;
 	}
 
-	
-	
-	
 }
