a328b9f7a2e99c28bc4c10c6df83c966cb996023
hemaassen <helgemaassen@yahoo.de>
2017-03-18 16:51:23 +0100

    HelpWindow und HelpWindowController hinzugefügt

---
+++ src/application/controller/HelpWindowController.java
@@ -0,0 +1,23 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+
+public class HelpWindowController {
+
+    @FXML
+    private Tab helpGeneral;
+
+    @FXML
+    private Tab helpManual;
+
+    @FXML
+    private Tab helpAutomatic;
+
+    @FXML
+    private Tab helpSearch;
+
+    @FXML
+    private Tab helbConfig;
+
+}
