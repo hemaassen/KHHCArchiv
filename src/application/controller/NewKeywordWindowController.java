08f973b881952ed028f406381bbec6d104636b88
hemaassen <helgemaassen@yahoo.de>
2017-03-21 13:08:58 +0100

    Button in NewKeyword mit ID belegt

--- src/application/controller/NewKeywordWindowController.java
+++ src/application/controller/NewKeywordWindowController.java
@@ -1,9 +1,16 @@
 package application.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class NewKeywordWindowController {
+
+	@FXML
+	private Button newKeywordCancel;
+
+	@FXML
+	private Button newKeywordSave;
 
 	@FXML
 	private TextField newKeywordTextField;
