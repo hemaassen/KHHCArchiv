de48beb174862169b20437d7e0a7eae43f5a1129
hemaassen <helgemaassen@yahoo.de>
2017-03-21 12:30:15 +0100

    NewKeyWord DialoOane hinzugefügt

---
+++ src/application/controller/NewKeywordWindowController.java
@@ -0,0 +1,11 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class NewKeywordWindowController {
+
+	@FXML
+	private TextField newKeywordTextField;
+
+}
